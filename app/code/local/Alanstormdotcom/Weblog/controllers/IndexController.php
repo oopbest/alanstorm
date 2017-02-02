e07b8db424e5b3075c029c0a8471c8fd5f654da2
Setthawut <oop_best@yahoo.com>
2017-02-02 18:37:49 +0700

    init

---
+++ app/code/local/Alanstormdotcom/Weblog/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+    class Alanstormdotcom_Weblog_IndexController extends Mage_Core_Controller_Front_Action {    
+        // public function testModelAction() {
+        //     $blogpost = Mage::getModel('weblog/blogpost');
+        //     echo get_class($blogpost);
+        // }
+        public function testModelAction() {
+            $params = $this->getRequest()->getParams();
+            $blogpost = Mage::getModel('weblog/blogpost');
+            echo("Loading the blogpost with an ID of ".$params['id']);
+            $blogpost->load($params['id']);        
+            $data = $blogpost->getData();
+            var_dump($data);    
+        }
+
+    }
